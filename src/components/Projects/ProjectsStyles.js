94383eba9b0c323f915767f558a857e26a61b0d8
Yosef <yosethio@yahoo.com>
2023-06-06 10:31:50 +0300

    hight 40 percent

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Img = styled.img`
 	width: 100%;
-	height: 100%;
+	height: 40%;
 	object-fit: cover;
 	overflow: hidden;
 	transition: transform 0.5s ease-in-out;
